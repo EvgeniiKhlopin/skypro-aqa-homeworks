ddb6892fdafd990ace708692203e597853c3d139
EvgeniiKhlopin <evgenii.khlopin@gmail.com>
2024-03-02 16:33:40 +0300

    update

--- ДЗ 2/lesson_2_task_3.py
+++ ДЗ 2/lesson_2_task_3.py
@@ -1,5 +1,6 @@
-side_of_a_square = int(input("Введите сторону квадрата: "))
-def square(result):
-   result = round (side_of_a_square * side_of_a_square)
+def square(side : int):
+   result = round(side * side)
+   return result
 
-print(square)
+n = int(input('Введите сторону квадрата: '))
+print(square(n))
