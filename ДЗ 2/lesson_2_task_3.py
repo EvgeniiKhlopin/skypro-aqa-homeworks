63130d6045fa51310fa7a4a875bd49613df4477d
EvgeniiKhlopin <evgenii.khlopin@gmail.com>
2024-03-06 20:15:41 +0300

    upd

--- ДЗ 2/lesson_2_task_3.py
+++ ДЗ 2/lesson_2_task_3.py
@@ -1,6 +1,13 @@
-def square(side : int):
-   result = round(side * side)
-   return result
+import math
 
-n = int(input('Введите сторону квадрата: '))
-print(square(n))
+def square(side):
+    if isinstance(side, float):
+        area = math.ceil(side*side)
+    elif isinstance(side, int):
+        area = side * side
+    else:
+        raise ValueError("Сторона не является числом.")
+    return print(area)
+
+square(10)
+square(1.1)
